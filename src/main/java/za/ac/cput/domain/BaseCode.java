a28139e04f78d743924b6042a67f60ada7f66aef
Tsireledzo Makhado <makhtsire29@gmail.com>
2023-03-12 21:19:18 +0200

    Basecode and dependencies added

---
+++ src/main/java/za/ac/cput/domain/BaseCode.java
@@ -0,0 +1,9 @@
+/**
+ * Main.Java
+ * This is main class used for basecode- so that src folder is added in the repo
+ * @author Tsireledzo Wisdom Makhado - 221116273
+ */
+package za.ac.cput.domain;
+
+public class BaseCode {
+}
